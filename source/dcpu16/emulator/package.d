d09c300864b98c22dad39026f035f8d8496a0d86
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-13 18:34:03 +0700

    test fail

--- source/dcpu16/emulator/package.d
+++ source/dcpu16/emulator/package.d
@@ -94,33 +94,29 @@ unittest
 
 unittest
 {
-    auto c = new Computer();
-    c.load =
+    auto comp = new Computer();
+    comp.load =
         [
             // high-nerd.dasm16
             0x7c41, 0x01f4, // SET C, 500      ; C = 500
             0x7c42, 0x01f3, // ADD C, 499      ; C = 999
             0x7c43, 0x0063, // SUB C, 99       ; C = 900
-            0x8c44,       // MUL C, 2        ; C = 1800
-            0x8001,       // SET A, 0xFFFF   ; C = 1800, A = 0xFFFF
-            0x0803,       // SUB A, C        ; C = 1800, A = 0xF8F8
-            0x0041,       // SET C, A        ; C = -1800, A = 0xF8F8
-            0x8401,       // SET A, 0        ; C = -1800
-            0x8c45,       // MLI C, 2        ; C = -3600
-            0x9047,       // DVI C, 3        ; C = -1200 (0xFB50)
+            0x8c44,         // MUL C, 2        ; C = 1800
+            0x8001,         // SET A, 0xFFFF   ; C = 1800, A = 0xFFFF
+            0x0803,         // SUB A, C        ; C = 1800, A = 0xF8F8
+            0x0041,         // SET C, A        ; C = -1800, A = 0xF8F8
+            0x8401,         // SET A, 0        ; C = -1800
+            0x8c45,         // MLI C, 2        ; C = -3600
+            0x9047,         // DVI C, 3        ; C = -1200 (0xFB50)
         ];
 
-    c.cpu.reset;
+    comp.cpu.reset;
 
     import std.stdio;
 
-    foreach(i; 0 .. 10)
+    with(comp.cpu.regs)
     {
-        writeln("Loaded "~c.cpu.ins.toString);
-        writeln(c.cpu.regs);
-        writeln("Memory: "~c.memDump);
-        writeln("Stack:\n"~c.cpu.stackDump);
-        c.cpu.step;
-        writefln("Step %d executed", i);
+        comp.cpu.step; writeln(c); assert(c == 500);
+        comp.cpu.step; assert(c == 999);
     }
 }
