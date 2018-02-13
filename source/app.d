d1f0ecc4942c1a490161977958f644c27f390f3e
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-14 01:42:45 +0700

    dlangui dependency added

--- source/app.d
+++ source/app.d
@@ -1,6 +1,28 @@
-import std.stdio;
+import dlangui;
 
-void main()
+mixin APP_ENTRY_POINT;
+
+extern (C) int UIAppMain(string[] args)
 {
-	writeln("Edit source/app.d to start your project.");
+    Window window = Platform.instance.createWindow("DCPU-16 emulator", null);
+
+    window.mainWidget = parseML(q{
+        VerticalLayout
+        {
+            TextWidget { text: "Hello World example for DlangUI"; textColor: "red"; fontSize: 150%; fontWeight: 800; fontFace: "Arial" }
+            // arrange controls as form - table with two columns
+
+            HorizontalLayout {
+                Button { id: btnOk; text: "Ok" }
+                Button { id: btnCancel; text: "Cancel" }
+            }
+        }
+    });
+
+
+    // show window
+    window.show();
+
+    // run message loop
+    return Platform.instance.enterMessageLoop();
 }
