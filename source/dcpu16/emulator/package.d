31103ca5772912f7f5da5efe792586e6317551c0
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-13 18:31:06 +0700

    another test added

--- source/dcpu16/emulator/package.d
+++ source/dcpu16/emulator/package.d
@@ -48,6 +48,13 @@ class Computer
         }
     }
 
+    void load(ushort[] from) pure
+    {
+        assert(from.length <= mem.length);
+
+        mem[0 .. from.length] = from;
+    }
+
     string memDump() const
     {
         import std.string;
@@ -63,8 +70,6 @@ class Computer
 
 unittest
 {
-    import std.stdio;
-
     auto c = new Computer();
     c.mem[0 .. 3] =
         [
@@ -72,11 +77,42 @@ unittest
             0x9002, // add a, 3 :loop
             0x8b81, // set pc, loop
         ];
-    //~ c.load("examples/loop.bin");
 
     assert(c.mem[0] != 0, "First instruction is null");
 
     c.cpu.reset;
+
+    foreach(i; 0 .. 16)
+        c.cpu.step;
+
+    assert(c.cpu.regs.A == 0x19);
+    assert(c.cpu.regs.pc == 0x02);
+
+    c.reset;
+    assert(c.mem[0] == 0);
+}
+
+unittest
+{
+    auto c = new Computer();
+    c.load =
+        [
+            // high-nerd.dasm16
+            0x7c41, 0x01f4, // SET C, 500      ; C = 500
+            0x7c42, 0x01f3, // ADD C, 499      ; C = 999
+            0x7c43, 0x0063, // SUB C, 99       ; C = 900
+            0x8c44,       // MUL C, 2        ; C = 1800
+            0x8001,       // SET A, 0xFFFF   ; C = 1800, A = 0xFFFF
+            0x0803,       // SUB A, C        ; C = 1800, A = 0xF8F8
+            0x0041,       // SET C, A        ; C = -1800, A = 0xF8F8
+            0x8401,       // SET A, 0        ; C = -1800
+            0x8c45,       // MLI C, 2        ; C = -3600
+            0x9047,       // DVI C, 3        ; C = -1200 (0xFB50)
+        ];
+
+    c.cpu.reset;
+
+    import std.stdio;
 
     foreach(i; 0 .. 10)
     {
@@ -87,6 +123,4 @@ unittest
         c.cpu.step;
         writefln("Step %d executed", i);
     }
-
-    c.reset;
 }
