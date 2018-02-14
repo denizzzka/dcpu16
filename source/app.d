ba548410e2c2508371646b8022275598e1dc8a9d
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-15 04:11:30 +0700

    Pause and Step buttons

--- source/app.d
+++ source/app.d
@@ -2,6 +2,8 @@ import dlangui;
 
 mixin APP_ENTRY_POINT;
 
+import dcpu16.emulator;
+
 extern (C) int UIAppMain(string[] args)
 {
     Window window = Platform.instance.createWindow("DCPU-16 emulator", null);
@@ -14,44 +16,41 @@ extern (C) int UIAppMain(string[] args)
             //~ ImageWidget { minWidth: 256; minHeight: 128; id: EMUL_0 }
 
             HorizontalLayout {
-                Button { id: btnOk; text: "Ok" }
-                Button { id: btnCancel; text: "Cancel" }
+                Button { id: PAUSE; text: "Pause" }
+                Button { id: STEP; text: "Step" }
             }
         }
     });
 
-    auto emulScr = new EmulatorScreenWidget("EMUL_SCREEN0");
-    window.mainWidget.insertChild(emulScr, 1);
 
-    import dcpu16.emulator;
     import dcpu16.emulator.devices.lem1802;
+    import std.stdio;
 
     auto comp = new Computer;
     auto disp = new LEM1802(comp);
     comp.attachDevice = disp;
 
-    enum blob = import("test.bin");
-    comp.load(cast(ubyte[]) blob);
+    auto emulScr = new EmulatorScreenWidget("EMUL_SCREEN0", comp, disp);
+    window.mainWidget.insertChild(emulScr, 1);
+    emulScr.setTimer(100);
 
-    foreach(_; 0 .. 40000)
-    {
-        //~ import std.stdio;
-        //~ comp.machineState.writeln;
-        comp.cpu.step;
-    }
+    window.mainWidget.childById("PAUSE").addOnClickListener((Widget) {
+            emulScr.isPaused = !emulScr.isPaused;
+            return true;
+        });
 
-    disp.forEachPixel(
-        (x, y, c)
-        {
-            auto rgba = makeRGBA(
-                    c.r * 17,
-                    c.g * 17,
-                    c.b * 17,
-                    0
-                );
-            emulScr.cdbuf.drawPixel(x, y, rgba);
-        }
-    );
+    window.mainWidget.childById("STEP").addOnClickListener((Widget) {
+            emulScr.comp.cpu.step;
+            comp.machineState.writeln;
+            return true;
+        });
+
+    ushort[] scrFill =
+    [
+        0x7c01, 0xf000, 0x7c21, 0x8000, 0x0121, 0x8802, 0x8822, 0x7c32,
+        0x8180, 0x7f81, 0x000d, 0x7f81, 0x0004, 0x7f81, 0x000d
+    ];
+    comp.load(scrFill);
 
     // show window
     window.show();
@@ -68,8 +67,11 @@ import dcpu16.emulator.devices.lem1802;
 class EmulatorScreenWidget : ImageWidget
 {
     public ColorDrawBuf cdbuf;
+    private Computer comp;
+    private LEM1802 display;
+    bool isPaused = true;
 
-    this(string id)
+    this(string id, Computer c, LEM1802 d)
     {
         super(id);
         minWidth = 256;
@@ -80,6 +82,39 @@ class EmulatorScreenWidget : ImageWidget
 
         Ref!DrawBuf r = cdbuf ;
         drawable = new ImageDrawable(r);
+
+        comp = c;
+        display = d;
+    }
+
+    override bool onTimer(ulong id)
+    {
+        import std.stdio;
+
+        if(!isPaused)
+        {
+            comp.cpu.step;
+            comp.machineState.writeln;
+        }
+
+        return true;
+    }
+
+    private void refreshFrameToBuf()
+    {
+        display.forEachPixel(
+            (x, y, c)
+            {
+                auto rgba = makeRGBA(
+                        c.r * 17,
+                        c.g * 17,
+                        c.b * 17,
+                        0
+                    );
+
+                cdbuf.drawPixel(x, y, rgba);
+            }
+        );
     }
 
     override void onDraw(DrawBuf buf)
