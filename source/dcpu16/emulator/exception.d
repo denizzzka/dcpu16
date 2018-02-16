7ac4d45e63e408c13baaa307bbbe28fc70c95279
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-16 10:16:01 +0700

    exception msg improved

--- source/dcpu16/emulator/exception.d
+++ source/dcpu16/emulator/exception.d
@@ -1,12 +1,17 @@
 module dcpu16.emulator.exception;
 
 import std.exception;
+import std.string;
 import dcpu16.emulator.cpu: Instruction;
 
 class Dcpu16Exception : Exception
 {
     this(string msg, Instruction ins, string file, size_t line) pure
     {
-        super(msg~"\n"~ins.toString, file, line);
+        super(
+            format("%s\n%s", msg, ins.toString),
+            file,
+            line
+        );
     }
 }
