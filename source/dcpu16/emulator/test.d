b39b702b3bf448da8c9ce382a046c3d7477cb1ef
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-16 09:25:22 +0700

    new test added

--- source/dcpu16/emulator/test.d
+++ source/dcpu16/emulator/test.d
@@ -16,3 +16,23 @@ unittest
         comp.cpu.step;
     }
 }
+
+unittest
+{
+    enum str = import("self-copy.bin");
+    auto blob = cast(ubyte[]) str;
+
+    auto comp = new Computer();
+    comp.load(blob, true);
+
+    foreach(_; 0 .. 4000)
+    {
+        //~ import std.stdio;
+        //~ comp.machineState.writeln;
+        //~ comp.cpu.step;
+    }
+
+    import std.algorithm.comparison;
+
+    //~ assert(equal(comp.mem[0 .. blob.length], comp.mem[blob.length .. blob.length*2]));
+}
