c5d95a83ebeb199c2ebe78ad30058a5f53dc03e1
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-16 18:00:38 +0700

    asm.d added, backup

---
+++ source/dcpu16/emulator/asm.d
@@ -0,0 +1,15 @@
+module dcpu16.emulator.asm_code;
+
+import dcpu16.emulator.cpu;
+
+pure:
+
+string explainOperand(ushort operand)
+{
+    return "";  
+}
+
+string explainValue(ushort operand)
+{
+    return "";
+}
